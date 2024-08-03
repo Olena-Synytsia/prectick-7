659e76ef8bb20aae94eb611781405037ae3b3061
Roman Zhelnov <roman95zhelnov@gmail.com>
2024-08-03 11:45:54 +0300

    add

--- src/js/api.js
+++ src/js/api.js
@@ -4,8 +4,26 @@ const BASE_URL = 'https://64859281a795d24810b7146a.mockapi.io/work';
 
 export async function getContacts() {
   try {
-    const data = await axios.get(`${BASE_URL}/contacts`);
-    return data.data;
+    const { data } = await axios.get(`${BASE_URL}/contacts`);
+    return data;
+  } catch {
+    console.log(error);
+  }
+}
+
+export async function getIdContact(Id) {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/contacts/${Id}`);
+    return data;
+  } catch {
+    console.log(error);
+  }
+}
+
+export async function createContact(body) {
+  try {
+    const { data } = await axios.post(`${BASE_URL}/contacts`, body);
+    return data;
   } catch {
     console.log(error);
   }

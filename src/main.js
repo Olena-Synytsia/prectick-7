9118f1a6e65ef8fd2a1f2757a20c7e09a0cacc9f
Roman Zhelnov <roman95zhelnov@gmail.com>
2024-08-03 12:59:48 +0300

    swiper

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import {
   putContact,
   deletContact,
 } from './js/api';
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-coverflow';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 
 getContacts().then(data => console.log(data));
 getIdContact(26).then(data => console.log(data));
@@ -13,3 +19,20 @@ getIdContact(26).then(data => console.log(data));
 // );
 // putContact(26, { name: 'Oscar' });
 deletContact(26);
+
+const swiper = new Swiper('.mySwiper', {
+  // configure Swiper to use modules
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: true,
+  modules: [EffectCoverflow, Pagination, Navigation],
+});
